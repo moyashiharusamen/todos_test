213a867a684ed4ec2ce9ced7023036d07061e4d1
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-08-10 10:22:13 +0900

    feat: supabase から取得するデータに型指定を追加

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export type Database = {
+  id: number;
+  title: string;
+}
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
